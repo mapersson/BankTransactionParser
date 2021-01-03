8ab12829d3c47a18ef230ceecfbccbb95e69d467
mapersson <62900241+mapersson@users.noreply.github.com>
2021-01-03 12:30:02 -0700

    Added SwiftCSV Pkg
    Removed CSV parsing code and integrated SwiftCSV pkg to do CSV parsing

--- Package.swift
+++ Package.swift
@@ -4,24 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "BankTransactionParser",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BankTransactionParser",
-            dependencies: ["Core"]
-            ),
-        .target(name: "Core",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
-            ),
-        .testTarget(
-            name: "BankTransactionParserTests",
-            dependencies: ["BankTransactionParser"]),
-    ]
+  name: "BankTransactionParser",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
+    .package(url: "https://github.com/swiftcsv/SwiftCSV", .upToNextMinor(from: "0.6.0")),
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "BankTransactionParser",
+      dependencies: ["Core"]
+    ),
+    .target(
+      name: "Core",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "SwiftCSV", package: "SwiftCSV"),
+      ]
+    ),
+    .testTarget(
+      name: "BankTransactionParserTests",
+      dependencies: ["BankTransactionParser"]),
+  ]
 )
--- Sources/Core/CreditCardTransactionParser.swift
+++ Sources/Core/CreditCardTransactionParser.swift
@@ -2,113 +2,48 @@ import ArgumentParser
 
 import Foundation
 
+import SwiftCSV
+
 public struct CreditCardTransactionParser: ParsableCommand {
   @Argument(help: "Location of file")
   var fileLocation: String
 
   public mutating func run() throws {
-    guard let raw = readFile(at: self.fileLocation) else {
+    guard let rawString = readFile(at: self.fileLocation) else {
       throw ExitCode.failure
     }
-    var trans = splitTransactionsFrom(string: raw)
-    print(trans)
-    trans.removeFirst(2)
-    let transa = transactionsFrom(array: trans)
-    for tr in transa {
-      print(tr.description)
 
+    var transactions = [TangTransaction]()
+    for row in rawString.namedRows {
+      transactions.append(TangTransaction(from: row))
     }
+    var output = ""
+    for tr in transactions {
+      print(tr.description)
+      output = output + tr.description + "\n"
+    }
+    #if !DEBUG
+      write(transations: output, to: "transactions.csv")
+    #endif
   }
 
   public init() {
   }
 
-  func readFile(at: String) -> String? {
+  func readFile(at: String) -> CSV? {
     do {
-      let rawString = try String(contentsOf: URL(fileURLWithPath: fileLocation))
-      return rawString
-    } catch {
+      let csvFile: CSV = try CSV(url: URL(fileURLWithPath: fileLocation))
+      return csvFile
+      // let rawString = try String(contentsOf: URL(fileURLWithPath: fileLocation))
+      // return rawString
+    }  // catch parseError as CSVParseError {
+    //   print("error")
+    //   return nil
+    // }
+    catch {
       print(error.localizedDescription)
       return nil
     }
-
-  }
-
-  func splitTransactionsFrom(string: String) -> [String] {
-    let transactions = string.components(separatedBy: .newlines).filter({ !$0.isEmpty })
-    return transactions
-  }
-
-  func transactionsFrom(array: [String]) -> [Transaction] {
-    var allTrans = [Transaction]()
-    var output = ""
-    for trans in array {
-      let components = trans.components(separatedBy: ",")
-      let date = parseDate(from: components.first!)
-      let amount = parseAmount(from: components.last!)
-      let merchant = parseMerchant(from: components[2])
-      let tr = Transaction(date: date!, merchant: merchant, amount: amount!)
-      print(tr.description)
-      output = output + tr.description + "\n"
-      allTrans.append(tr)
-    }
-    write(transations: output, to: "myLocation")
-    return allTrans
-  }
-
-  /**
- Parses the date information from the provided string.
- - Parameter date:  A string that contains the date information
- - Returns: A Date instance that contains the correct day, month and year. If there is an error parsing the string, nil is returned.
- - Precondition: The date string is in the format `M-d-yyyy`.
- */
-  public func parseDate(from date: String) -> Date? {
-
-    guard !date.isEmpty else { return nil }
-
-    let trimmedDate = date.trimmingCharacters(in: .whitespaces)
-
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.dateFormat = "M-d-yyyy"
-
-    var returnDate = dateFormatter.date(from: trimmedDate)
-    #if DEBUG
-      returnDate = Date()
-    #endif
-    return returnDate
-
-  }
-
-  /// Parses the transaction amout from the provided string
-  /// - Parameter string: A string representing the transaction amount.
-  /// - Returns: A Double instance that contains the transaction amount. If there is a parsing error, nil is returned
-  func parseAmount(from string: String) -> Double? {
-    guard !string.isEmpty else {
-      return nil
-    }
-    //		TODO: Add error handling in the event the string can not be parsed.
-    let trimmed = string.trimmingCharacters(in: .whitespaces)
-    return Double(trimmed)
-  }
-
-  func parseMerchant(from merchant: String) -> String {
-    let trailingPatrn = #"([ #][ 0-9]+[\s\w]*|[-\d ]*|[\*\w]*) [\S]*$"#
-    let trailingRegex = try! NSRegularExpression(
-      pattern: trailingPatrn, options: .anchorsMatchLines)
-    let stringRange = NSRange(location: 0, length: merchant.utf16.count)
-    let substitutionString = #""#
-    let trailingRemoved = trailingRegex.stringByReplacingMatches(
-      in: merchant, range: stringRange, withTemplate: substitutionString)
-
-    let dupLocPatrn = #"^([\w ]+) (BANFF|CANMORE|DEAD MANS)"#
-    let dupLocRegex = try! NSRegularExpression(pattern: dupLocPatrn, options: .anchorsMatchLines)
-    let dupLocRange = NSRange(location: 0, length: trailingRemoved.utf16.count)
-    let dupLocSubstString = #"$1"#
-    var final = dupLocRegex.stringByReplacingMatches(
-      in: trailingRemoved, range: dupLocRange, withTemplate: dupLocSubstString)
-    final = final.trimmingCharacters(in: .whitespaces)
-    return MerchantDict.lookup(merchant: final)
   }
 
   func write(transations: String, to location: String) {
@@ -126,25 +61,4 @@ public struct CreditCardTransactionParser: ParsableCommand {
     return paths[0]
   }
 
-  enum CCTPErrors: Error {
-    case fileNotFound
-    case fileNotRead
-  }
-}
-
-extension NSRegularExpression {
-  convenience init(_ pattern: String) {
-    do {
-      try self.init(pattern: pattern)
-    } catch {
-      preconditionFailure("Illegal regular expression: \(pattern).")
-    }
-  }
-}
-
-extension NSRegularExpression {
-  func matches(_ string: String) -> Bool {
-    let range = NSRange(location: 0, length: string.utf16.count)
-    return firstMatch(in: string, options: [], range: range) != nil
-  }
 }
