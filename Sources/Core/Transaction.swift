2aaf0fe4a6f906024afb0083c2274c54d8c9a6a1
mapersson <62900241+mapersson@users.noreply.github.com>
2021-01-03 12:34:50 -0700

    Update Transaction.swift
    Created a Tang Transaction struct based on the generic Transaction struct. Moved parsing functions associated with creating a transaction to this file.

--- Sources/Core/Transaction.swift
+++ Sources/Core/Transaction.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-struct Transaction {
+private struct Transaction {
   var date: Date
-  var merchant: String
+  var merchant: Merchant
   var amount: Double
 
   var description: String {
@@ -16,7 +16,80 @@ struct Transaction {
     numberFormatter.maximumFractionDigits = 2
     let formattedAmount = numberFormatter.string(from: NSNumber(value: amount)) ?? "0.00"
 
-    return "\(formattedDate),\(merchant),\(formattedAmount)"
+    return "\(formattedDate),\(merchant.name),\(formattedAmount)"
   }
+
+}
+
+struct TangTransaction: BankTransaction {
+  private let _transaction: Transaction
+  private enum colNames: String {
+    case date = "Transaction date"
+    case merchantName = "Name"
+    case amount = "Amount"
+  }
+  var date: Date {
+    return _transaction.date
+  }
+  var merchant: String {
+    return _transaction.merchant.name
+  }
+
+  var amount: Double {
+    return _transaction.amount
+  }
+
+  var description: String {
+    _transaction.description
+  }
+
+  init(from dict: [String: String]) {
+    let _date = parseDate(from: dict[colNames.date.rawValue]!) ?? Date()
+    let _merchant = Merchant(from: dict[colNames.merchantName.rawValue]!)
+    let _amount = parseAmount(from: dict[colNames.amount.rawValue]!) ?? 0.0
+    _transaction = Transaction(date: _date, merchant: _merchant, amount: _amount)
+  }
+}
+
+/// Parses the date information from the provided string.
+/// - Parameter date:  A string that contains the date information
+/// - Returns: A Date instance that contains the correct day, month and year. If there is an error parsing the string, nil is returned.
+/// - Precondition: The date string is in the format `M-d-yyyy`.
+private func parseDate(from date: String) -> Date? {
+
+  guard !date.isEmpty else { return nil }
+
+  let trimmedDate = date.trimmingCharacters(in: .whitespaces)
+
+  let dateFormatter = DateFormatter()
+  dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+  dateFormatter.dateFormat = "M-d-yyyy"
+
+  var returnDate = dateFormatter.date(from: trimmedDate)
+  #if DEBUG
+    returnDate = Date()
+  #endif
+  return returnDate
+
+}
+
+/// Parses the transaction amout from the provided string
+/// - Parameter string: A string representing the transaction amount.
+/// - Returns: A Double instance that contains the transaction amount. If there is a parsing error, nil is returned
+private func parseAmount(from string: String) -> Double? {
+  guard !string.isEmpty else {
+    return nil
+  }
+  let trimmed = string.trimmingCharacters(in: .whitespaces)
+  return Double(trimmed)
+}
+
+protocol BankTransaction {
+  var date: Date { get }
+  var merchant: String { get }
+  var amount: Double { get }
+  var description: String { get }
+
+  init(from dict: [String: String])
 
 }
